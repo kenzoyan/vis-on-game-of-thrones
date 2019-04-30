21d9a87393aec75f9e5427400074509c3dddce77
Kenzo <570704104@qq.com>
2019-04-30 18:58:30 +0800

    add control bar

--- A3.py
+++ A3.py
@@ -7,13 +7,14 @@ data=pd.read_csv("got_csv.csv")
 dropindex=['Director','Writer','Original Air Date','Runtime (mins)','IMDB Description', 'IMDB votes']
 minidata=data.drop(dropindex,axis=1)
 minidata.head()   
+minidata['L']=1
 
 selector=alt.selection_multi(empty='all', fields=['Season'])
-
+#input_dropdown = alt.binding_select(options=['1','2','3','4','5','6','7'])
+#selector = alt.selection_single(fields=['Season'], bind=input_dropdown, name='Country of ')
 base=alt.Chart(minidata).properties(
     width=600,
     height=600,
-    title='Vis on Game of Thrones',
     ).add_selection(
         selector
     )
@@ -26,22 +27,37 @@ left=base.mark_point(filled=True,fillOpacity=0.9,size=400).encode(
     color=alt.condition(selector, 'Season:N', alt.value('lightgray')),
     shape='Season:N',
     tooltip=['US viewers (million)', 'Notable Death Count', 'Imdb Rating', 'Season']
-    ).interactive()
+    ).interactive().properties(
+        title='Scatter--Vis on Game of Thrones',
+    )
 
 line=base.mark_line(point=True,).encode(
     alt.X('Number in Season:Q'),
     alt.Y('Imdb Rating:Q',scale=alt.Scale(domain=(7.5, 10))),
-    color=alt.Color('Season:N')
-    ).transform_filter(
+    color=alt.condition(selector, 'Season:N', alt.value('transparent')),
+    #color=alt.Color('Season:N')
+    ).add_selection(
     selector
+    ).properties(
+        title='Line--Vis on Game of Thrones',
     )
 
-left.configure_title(
+label=alt.Chart(minidata).mark_circle(size=200,filled=True).encode(
+    alt.X('L:N'),
+    alt.Y('Season:N'),
+    color=alt.condition(selector, 'Season:N', alt.value('lightgray')),
+    ).add_selection(
+    selector
+    ).properties(
+    title="Control Bar"
+    )
+
+base.configure_title(
     fontSize=20,
     font='Courier',
     anchor='start',
     color='gray'
 )
 
-all=left|line
+all=left|label|line
 all.save("index.html")
